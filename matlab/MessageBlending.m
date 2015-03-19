84390c8b7c2c593ebb341528b1aad4da97863077
Revan Sopher <rsopher@gmail.com>
2015-03-19 17:18:15 -0400

    Sample blending script.

---
+++ matlab/MessageBlending.m
@@ -0,0 +1,115 @@
+
+
+blocksize = 25;
+nblock = 3; % Don't change this
+repeatsize = 5; % increases blurring between borders
+
+blurfilt = conv2([1 4 6 4 1], [1 4 6 4 1]')/256;
+for i = 1:repeatsize, blurfilt = conv2(blurfilt,blurfilt); end;
+
+img = zeros(nblock*blocksize,nblock*blocksize);
+border = 5;
+border = 0;
+
+zblock = zeros(blocksize,blocksize);
+oblock = zeros(blocksize,blocksize);
+
+for ii = 1:blocksize
+    for jj = 1:blocksize
+oblock(ii,jj) = (ii > border & jj > border & ii < (blocksize - border) & jj < (blocksize - border));
+    end
+end
+
+
+
+
+mask = zeros(9,nblock*blocksize,nblock*blocksize);
+mask(1,:,:) = [ oblock zblock zblock; zblock zblock zblock; zblock zblock zblock];
+mask(2,:,:) = [ zblock oblock zblock; zblock zblock zblock; zblock zblock zblock];
+mask(3,:,:) = [ zblock zblock oblock; zblock zblock zblock; zblock zblock zblock];
+mask(4,:,:) = [ zblock zblock zblock; oblock zblock zblock; zblock zblock zblock];
+mask(5,:,:) = [ zblock zblock zblock; zblock oblock zblock; zblock zblock zblock];
+mask(6,:,:) = [ zblock zblock zblock; zblock zblock oblock; zblock zblock zblock];
+mask(7,:,:) = [ zblock zblock zblock; zblock zblock zblock; oblock zblock zblock];
+mask(8,:,:) = [ zblock zblock zblock; zblock zblock zblock; zblock oblock zblock];
+mask(9,:,:) = [ zblock zblock zblock; zblock zblock zblock; zblock zblock oblock];
+
+% check it
+for i = 1:9,
+currentmask = reshape(mask(i,:,:),nblock*blocksize,nblock*blocksize); figure(1);imagesc(currentmask);pause;
+end;
+
+% Blurring
+for i = 1:9,
+    currentmask = reshape(mask(i,:,:),nblock*blocksize,nblock*blocksize); 
+    currentmask = conv2(currentmask,blurfilt,'same');
+    mask(i,:,:) = currentmask;
+end;
+
+% check it
+for i = 1:9,
+    currentmask = reshape(mask(i,:,:),nblock*blocksize,nblock*blocksize); figure(1);imagesc(currentmask);pause;
+end;
+
+
+
+ % The 9 masks encode the weight for each pixel ... normalize to that the
+ % 9x1 vector per pixel as defined by the mask sums to 1
+ 
+ ss  = nblock*blocksize;
+ for (ii = 1:ss),
+     for (jj = 1:ss)
+        normfactor(ii,jj) = sum(mask(:,ii,jj));
+     end
+ end
+ 
+ % for each pixel the nine masks define nine weights. Divide by normfactor
+ % so they sum to 1. 
+ 
+for ii = 1:9, 
+    currentmask = reshape(mask(ii,:,:),nblock*blocksize,nblock*blocksize);
+    currentmask = currentmask./normfactor;
+    mask(ii,:,:) = currentmask;
+end;
+
+
+
+% check it
+for i = 1:9,
+currentmask = reshape(mask(i,:,:),nblock*blocksize,nblock*blocksize); figure(1);imagesc(currentmask);pause;
+end;
+
+
+
+% Mask 1 defines the weights for upper left neighbor ...
+%
+%
+% Mask 9 defines the weights for the lower right neighbor
+
+% At this point we only need the weights in the center of the mask. 
+% so crop off the rest
+finalmask = mask(:,(blocksize+1):2*blocksize,(blocksize+1):2*blocksize);
+
+% check it
+for i = 1:9,
+currentmask = reshape(finalmask(i,:,:),blocksize,blocksize); figure(1);imagesc(currentmask);pause;
+end;
+
+
+
+
+
+% Now this is how you use the weights.
+% A message at pixel i,j is the weighted sum of the messages of 9 blocks (itself and
+% all its neighbors). The 9 masks give the weights of each of the message
+% blocks for every pixel. 
+
+
+
+
+
+
+
+
+
+
