47d1ef5725a997671445adac31603e78abbab1bf
Revan Sopher <rsopher@gmail.com>
2015-03-29 20:44:40 -0400

    NDK code for sliding window.

--- jni/native.cpp
+++ jni/native.cpp
@@ -1,6 +1,7 @@
 #include <jni.h>
 #include <string.h>
 #include <android/log.h>
+#include <stdlib.h>
 
 #include <cv.h>
 #include <highgui.h>
@@ -112,62 +113,153 @@ extern "C" {
 	 */
 	void extractMessage(Mat &image, jboolean *message,
 			int width, int height,
-			int width_blocks, int height_blocks) {
+			int width_blocks, int height_blocks,
+			int cutoff) {
 		int block_width = width / width_blocks;
 		int block_height = height / height_blocks;
 		
-		Scalar m = mean(image);
-		int cutoff = m[0] + m[1] + m[2];
-		//cutoff += 3 * 20;
-
 		int k = 0;
 		for (int i = 0; i < height; i += block_height) {
 			for (int j = 0; j < width; j += block_width) {
 				// Get region of interest
 				Mat block = image(Rect(i, j, block_height, block_width));
-				m = mean(block);
+				Scalar m = mean(block);
 				message[k++] = (m[0] + m[1] + m[2] > cutoff) ? true : false;
 			}
 		}
 	}
 
     /**
+	 * Determines two frames with most differing average intensity.
      * Subtracts frame2 from frame1, overwriting frame1
      */
     JNIEXPORT jbooleanArray Java_com_android_visualmimo_MainActivity_frameSubtraction(JNIEnv *env, jobject obj,
-        jbyteArray frame1, jbyteArray frame2,
+        jbyteArray frame1, jbyteArray frame2, jbyteArray frame3, jbyteArray frame4,
         jint width, jint height,
         jfloat c0x1, jfloat c0y1,
         jfloat c1x1, jfloat c1y1,
         jfloat c2x1, jfloat c2y1,
         jfloat c3x1, jfloat c3y1,
+
         jfloat c0x2, jfloat c0y2,
         jfloat c1x2, jfloat c1y2,
         jfloat c2x2, jfloat c2y2,
-        jfloat c3x2, jfloat c3y2
+        jfloat c3x2, jfloat c3y2,
+
+        jfloat c0x3, jfloat c0y3,
+        jfloat c1x3, jfloat c1y3,
+        jfloat c2x3, jfloat c2y3,
+        jfloat c3x3, jfloat c3y3,
+
+        jfloat c0x4, jfloat c0y4,
+        jfloat c1x4, jfloat c1y4,
+        jfloat c2x4, jfloat c2y4,
+        jfloat c3x4, jfloat c3y4
         )
     {
       //f1 and f2 are the byte data of the two frames, l1 and l2 are the array lengths
       jbyte* f1 = env->GetByteArrayElements(frame1, NULL);
       jbyte* f2 = env->GetByteArrayElements(frame2, NULL);
+      jbyte* f3 = env->GetByteArrayElements(frame3, NULL);
+      jbyte* f4 = env->GetByteArrayElements(frame4, NULL);
       jsize l1 = env->GetArrayLength(frame1);
 
       // Create OpenCV matrix from 1D array.
       Mat matImage1(l1, 1, CV_8UC1, (unsigned char *)f1);
       Mat matImage2(l1, 1, CV_8UC1, (unsigned char *)f2);
+      Mat matImage3(l1, 1, CV_8UC1, (unsigned char *)f3);
+      Mat matImage4(l1, 1, CV_8UC1, (unsigned char *)f4);
 
       // Reshape matrix. 3 layers, correct height.
       // NOTE(revan): still needs to be flipped and have colors reordered (see MATLAB script).
+	  //              This doesn't matter for message extraction though.
       Mat reshapedImage1 = matImage1.reshape(3, height);
       Mat reshapedImage2 = matImage2.reshape(3, height);
+      Mat reshapedImage3 = matImage3.reshape(3, height);
+      Mat reshapedImage4 = matImage4.reshape(3, height);
+	  matImage1.release();
+	  matImage2.release();
+	  matImage3.release();
+	  matImage4.release();
+
+	  // Find best pair of frames
+	  Scalar m;
+	  m = mean(reshapedImage1);
+	  int in1 = m[0] + m[1] + m[2];
+	  m = mean(reshapedImage2);
+	  int in2 = m[0] + m[1] + m[2];
+	  m = mean(reshapedImage3);
+	  int in3 = m[0] + m[1] + m[2];
+	  m = mean(reshapedImage4);
+	  int in4 = m[0] + m[1] + m[2];
+
+	  int pair_1_2 = abs(in1 - in2);
+	  int pair_1_3 = abs(in1 - in3);
+	  int pair_1_4 = abs(in1 - in4);
+	  int pair_2_3 = abs(in2 - in3);
+	  int pair_2_4 = abs(in2 - in4);
+	  int pair_3_4 = abs(in3 - in4);
+
+	  int threshold;
+
+	  Mat *good_image1;
+	  Mat *good_image2;
+	  if (pair_1_2 > pair_1_3
+			  && pair_1_2 > pair_1_4
+			  && pair_1_2 > pair_2_3
+			  && pair_1_2 > pair_2_4
+			  && pair_1_2 > pair_3_4) {
+		  good_image1 = &reshapedImage1;
+		  good_image2 = &reshapedImage2;
+		  reshapedImage3.release();
+		  reshapedImage4.release();
+		  threshold = (in1 + in2) / 2;
+	  } else if (pair_1_3 > pair_1_4
+			  && pair_1_3 > pair_2_3
+			  && pair_1_3 > pair_2_4
+			  && pair_1_3 > pair_3_4) {
+		  good_image1 = &reshapedImage1;
+		  good_image2 = &reshapedImage3;
+		  reshapedImage2.release();
+		  reshapedImage4.release();
+		  threshold = (in1 + in3) / 2;
+	  } else if (pair_1_4 > pair_2_3
+			  && pair_1_4 > pair_2_4
+			  && pair_1_4 > pair_3_4) {
+		  good_image1 = &reshapedImage1;
+		  good_image2 = &reshapedImage4;
+		  reshapedImage2.release();
+		  reshapedImage3.release();
+		  threshold = (in1 + in4) / 2;
+	  } else if (pair_2_3 > pair_2_4
+			  && pair_2_3 > pair_3_4) {
+		  good_image1 = &reshapedImage2;
+		  good_image2 = &reshapedImage3;
+		  reshapedImage1.release();
+		  reshapedImage4.release();
+		  threshold = (in2 + in3) / 2;
+	  } else if (pair_2_4 > pair_3_4) {
+		  good_image1 = &reshapedImage2;
+		  good_image2 = &reshapedImage4;
+		  reshapedImage3.release();
+		  reshapedImage4.release();
+		  threshold = (in2 + in4) / 2;
+	  } else {
+		  good_image1 = &reshapedImage3;
+		  good_image2 = &reshapedImage4;
+		  reshapedImage1.release();
+		  reshapedImage2.release();
+		  threshold = (in3 + in4) / 2;
+	  }
+
 
       // Define the destination image
       cv::Mat target1 = cv::Mat::zeros(width, height, CV_8UC1);
       cv::Mat target2 = cv::Mat::zeros(width, height, CV_8UC1);
-      projectiveTransform(reshapedImage1, target1, c0x1, c0y1, c1x1, c1y1, c2x1, c2y1, c3x1, c3y1);
-      projectiveTransform(reshapedImage2, target2, c0x2, c0y2, c1x2, c1y2, c2x2, c2y2, c3x2, c3y2);
+      projectiveTransform(*good_image1, target1, c0x1, c0y1, c1x1, c1y1, c2x1, c2y1, c3x1, c3y1);
+      projectiveTransform(*good_image2, target2, c0x2, c0y2, c1x2, c1y2, c2x2, c2y2, c3x2, c3y2);
 
-      imwrite("/sdcard/vmimo-frame1-noeq.bmp", target1);
+      //imwrite("/sdcard/vmimo-frame1-noeq.bmp", target1);
 
       histogramEqualization(target1);
       histogramEqualization(target2);
@@ -183,14 +275,11 @@ extern "C" {
 	  int height_blocks = 8;
 	  int num_blocks = width_blocks * height_blocks;
 	  jboolean message[num_blocks];
-	  extractMessage(target1, message, width, height, width_blocks, height_blocks);
+	  extractMessage(target1, message, width, height, width_blocks, height_blocks, threshold);
 
 	  // TODO: do something with the message
 	  jbooleanArray message_jboolean = env->NewBooleanArray(num_blocks);
 	  env->SetBooleanArrayRegion(message_jboolean, 0, num_blocks, message);
-	  //for (int i = 0; i < num_blocks; i++) {
-	  //    message_jboolean[i] = message[i];
-	  //}
 
       // Save to file
 //      flip(reshapedImage1.t(), reshapedImage1, 1);
@@ -202,6 +291,8 @@ extern "C" {
       // last arg: 0 -> copy array back, JNI_ABBORT -> don't copy
       env->ReleaseByteArrayElements(frame1, f1, JNI_ABORT);
       env->ReleaseByteArrayElements(frame2, f2, JNI_ABORT);
+      env->ReleaseByteArrayElements(frame3, f2, JNI_ABORT);
+      env->ReleaseByteArrayElements(frame4, f2, JNI_ABORT);
 
 	  return message_jboolean;
     }
