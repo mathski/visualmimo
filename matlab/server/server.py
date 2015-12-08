ff74a9aa8d7fa1b7cac7e9b563e10c40c4c90bc7
Revan Sopher <rsopher@gmail.com>
2015-12-08 03:27:58 -0500

    Fix web view by converting AVI to WEBM.

--- matlab/server/server.py
+++ matlab/server/server.py
@@ -1,13 +1,10 @@
-#!/bin/python3
+#!/usr/bin/python3
 # Trivial flask server to wrap MATLAB encoder and serve output
 # Remember to `source set_ld_path.sh` before running.
 
-from tornado.wsgi import WSGIContainer
-from tornado.httpserver import HTTPServer
-from tornado.ioloop import IOLoop
-
 from flask import Flask, send_file, render_template
 import os.path
+import subprocess
 import vmimo.server
 
 s = vmimo.server.initialize()
@@ -18,27 +15,30 @@ app = Flask(__name__)
 def root():
 	return 'access /encode/<imageid>/<asciistring>'
 
-@app.route('/dummy')
-def dummy():
-	return send_file('drop.avi', mimetype='video/avi')
-
 @app.route('/encode/<string:imageid>/<string:asciistring>')
 def encode(imageid, asciistring):
 	filename = imageid + '-' + asciistring + '.avi'
+	filename_webm = imageid + '-' + asciistring + '.webm'
 
 	print('encoding %s' % filename)
 
 	fullpath = os.path.join('..', filename)
-	# fullpath = filename
+	fullpath_webm = os.path.join('..', filename_webm)
 
 	if not os.path.isfile(filename):
-		# print(os.listdir())
+		print(os.listdir())
 		print('file (%s) not found, generating...' % filename)
 		s.sample(imageid, asciistring)
 		print('...generated!')
 
+	# note that I'm passing unsanitized input as parameters.
+	# This is astoundingly stupid, so never deploy this anywhere important.
+	command = 'LD_LIBRARY_PATH=/usr/lib ffmpeg -i %s %s -nostdin -y -hide_banner -nostats' % (filename, filename_webm)
+	print(command)
+	subprocess.call(command, shell=True)
 
-	return send_file(fullpath, mimetype='video/avi')
+
+	return send_file(fullpath_webm, mimetype='video/webm')
 
 @app.route('/view/<string:imageid>/<string:asciistring>')
 def view(imageid, asciistring):
@@ -47,10 +47,7 @@ def view(imageid, asciistring):
 
 
 if __name__ == '__main__':
+	print('starting!')
 	app.debug = True
-	app.use_x_sendfile = True
-	# app.run(threaded=True)
-
-	http_server = HTTPServer(WSGIContainer(app))
-	http_server.listen(5000)
-	IOLoop.instance().start()
+	#app.use_x_sendfile = True
+	app.run()
--- matlab/server/set_ld_path.sh
+++ matlab/server/set_ld_path.sh
@@ -1,5 +1,5 @@
-# Library paths for Fedora 22 x64. Use `source` to load this file.
-export MATLABROOT=/usr/local/MATLAB/R2015b
+# library paths configured for Ubuntu server 15.10 x64. Use `source` to load this file.
+export MATLABROOT=/usr/local/MATLAB/MATLAB_Runtime/v90/
 export LD_LIBRARY_PATH=$MATLABROOT/runtime/glnxa64/:$MATLABROOT/sys/os/glnxa64:$MATLABROOT/bin/glnxa64:$MATLABROOT/extern/lib/glnxa64:$MATLABROOT/sys/java/jre/glnxa64/jre1.5.0/lib/amd64/native_threads:$MATLABROOT/sys/java/jre/glnxa64/jre1.5.0/lib/amd64/server:$MATLABROOT/sys/java/jre/glnxa64/jre1.5.0/lib/amd64:$LD_LIBRARY_PATH
 export XAPPLRESDIR=$MATLABROOT/X11/app-defaults 
 
