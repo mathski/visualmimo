86988e83903df819ea285f654aaebc2d5f178471
Revan Sopher <rsopher@gmail.com>
2016-02-01 02:23:47 -0500

    Multiframe MATLAB encoding.

--- matlab/sample.m
+++ matlab/sample.m
@@ -1,10 +1,10 @@
-function [ message ] = sample(imageId, message, alpha, fps, height, width)
+function [ message ] = sample(imageId, messages, alpha, fps, height, width)
 % Sample main function that encodes a message as a checkerboard, applies it
 % to an image, and writes the result to file.
 % imageId: optional string corresponding to one of the images in `images/`.
 %          defaults to '5'.
-% message: optional string giving message to embed
-%          defaults to 'abcdefghijk'
+% message: optional cell array of strings giving message to embed
+%          defaults to {'abcdefghij'}
 
 if ~exist('alpha', 'var'), alpha = 10; end
 if ~exist('fps', 'var'), fps = 10; end
@@ -33,13 +33,9 @@ img=hsv2rgb(img);
 % img=uint8(img*255);
 
 
-if exist('message', 'var')
-    messages = {message};
-else
-    messages = {'abcdefghijk'};
+if ~exist('message', 'var')
+    messages = {'abcdefghij', 'lmnopqrstu'};
 end
-
-% messages = {'abcdefghijk'; 'lmnopqrstuv'};
 
 frames = [];
 totalmessage = '';
@@ -48,13 +44,13 @@ for i = 1:length(messages);
     message = asciiMessage(messages{i}, 80, i-1);
     
     % sync_bit for in-progress multiframe implementation
-    sync_bit = mod(i, 2);
-    if sync_bit == 0;
-        sync_bit = -1;
+    parity_bit = mod(i, 2);
+    if parity_bit == 0;
+        parity_bit = -1;
     end
 
-     img1=uint8(img * 255 + checkerboardEncoder(alpha, height, width, 8, 10, message, 0, sync_bit));
-     img2=uint8(img * 255 + checkerboardEncoder(alpha, height, width, 8, 10, message, 1, sync_bit));
+     img1=uint8(img * 255 + checkerboardEncoder(alpha, height, width, 8, 10, message, 0, parity_bit));
+     img2=uint8(img * 255 + checkerboardEncoder(alpha, height, width, 8, 10, message, 1, parity_bit));
     
 %     img1=uint8(img * 255 + colorEncoder(img, height, width, message, 0, sync_bit));
 %     img2=uint8(img * 255 + colorEncoder(img, height, width, message, 1, sync_bit));
