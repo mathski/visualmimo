d30501dfa1dc42c5b8391c99849eba5f7010fa94
joeb3219 <joe@websitebegin.com>
2016-03-30 18:30:13 -0400

    New matlab code from Eric

--- matlab/embedControllerImage.m
+++ matlab/embedControllerImage.m
@@ -1,41 +1,15 @@
 function  [apple_base, appleEmbedded] = embedControllerImage(apple, varargin)
-%% [apple_base, appleEmbedded, mymap] = matchPixelsAndEmbedImage(INPUT_IMAGE, MESSAGE(NxM matrix), DELTA_SIZE, BASE_COLOR_MAP, DELTA_COLOR_MAP)
+%% [apple_base, appleEmbedded, mymap] = embedControllerImage(INPUT_IMAGE, MESSAGE(NxM matrix), DELTA_SIZE, BASE_COLOR_MAP, DELTA_COLOR_MAP)
     % matchPixelsAndEmbedImage takes a base image and generates a pair of 
     % embedded images, along with a colormap used to make movie frames.
 
-    %% Resize input image to convenient size
-%   
-    img_size = size(apple);
-    disp(img_size);
-    if(img_size >= [360,640,3])
-        scale = max([360,640]./img_size(1:2));
-        apple = imresize(apple,scale);
-        apple = apple(1:360,1:640,:);
-        apple = double(apple)/255;
-    else
-        disp('Image too small!')
-        return
-    end
-    disp(size(apple));
-    %{
-    img_size = size(apple);
-    if(img_size >= [1152,2644,3])
-        scale = max([1152,2644]./img_size(1:2));
-        apple = imresize(apple,scale);
-        apple = apple(1:1152,1:2644,:);
-        apple = double(apple)/255;
-    else
-        disp('Image too small!')
-        return
-    end
-    %}
+
     %% Defaults and Variable Inputs
-    [imgHeight,imgWidth,~]=size(apple);
     
     if nargin == 1 % Default message and step size
         %%% Checkerboard Message
-        checkHeightBlocks = 8;
-        checkWidthBlocks = 10;
+        checkHeightBlocks = 8; %9  %CHANGED FOR DEBUGGING
+        checkWidthBlocks = 10; %16 %CHANGED FOR DEBUGGING
         genCheck = double(checkerboard(1,checkHeightBlocks,checkWidthBlocks) > 0.5);
         message = genCheck(1:(end/2),1:(end/2));
         %%% Delta step size
@@ -44,8 +18,7 @@ function  [apple_base, appleEmbedded] = embedControllerImage(apple, varargin)
         recal_colormap_flag = true;
     elseif nargin == 2 % Default Step Size
         %%% Checkerboard Message
-        message = to2DMatrix(varargin{1});
-        message1D = varargin{1};
+        message = varargin{1};
         %%% Delta step size
         delta_length = 5;
         %%% recalculate colormap flag
@@ -72,14 +45,46 @@ function  [apple_base, appleEmbedded] = embedControllerImage(apple, varargin)
         disp('Wrong number of inputs')
         return
     end
-    delta_length = 40;
     
-    [checkHeightBlocks,checkWidthBlocks] = size(message);
+        %% Resize input image to convenient size
+   
+        [checkHeightBlocks,checkWidthBlocks] = size(message);
+    
+        img_size = size(apple); %%%%%%%%%%%%% 360 x 640%%%%%%%%%%%%%%%%
+        imheight = checkHeightBlocks*70;
+        imwidth = checkWidthBlocks*70;
+        if(img_size >= [imheight,imwidth,3])
+            scale = max([imheight,imwidth]./img_size(1:2));
+            apple = imresize(apple,scale);
+            apple = apple(1:imheight,1:imwidth,:);
+            apple = double(apple)/255;
+        else
+            disp('Image too small!')
+            return
+        end
+        %{
+        img_size = size(apple);
+        if(img_size >= [1152,2644,3])
+            scale = max([1152,2644]./img_size(1:2));
+            apple = imresize(apple,scale);
+            apple = apple(1:1152,1:2644,:);
+            apple = double(apple)/255;
+        else
+            disp('Image too small!')
+            return
+        end
+        %}
+        
+    % Histogram Equalize Image
+%     apple_hsv = rgb2hsv(apple);
+%     apple_hsv_eq = histeq(apple_hsv(:,:,3));
+%     apple_hsv(:,:,3) = apple_hsv_eq;
+%     apple_rgb = hsv2rgb(apple_hsv);
+%     apple = apple_rgb;
+    
+    [imgHeight,imgWidth,~]=size(apple);
     checkPixelSize = imgHeight/checkHeightBlocks;
         
-    disp(message);
-    assignin('base', 'message', message);
-    
     %% Load Training Data - Labeled Color Pairs
     if(recal_colormap_flag)
         BER_threshold = 0;
@@ -147,14 +152,14 @@ function  [apple_base, appleEmbedded] = embedControllerImage(apple, varargin)
     
     %% Create Blended Checkrerboard Pattern
     % Create blending mask
-    mask = createBlendingMask(64);
-    heightstep  = 360/8;
-    widthstep   = 640 / 10;
-    widthnum    = 10;
-    heightnum   = 8;
+    mask = createBlendingMask(checkPixelSize);
+    
+    heightstep  = checkPixelSize;
+    widthstep   = checkPixelSize;
+    widthnum    = checkWidthBlocks;
+    heightnum   = checkHeightBlocks;
 
     blurred = zeros(imgHeight,imgWidth);
-    
     for h = 0:(heightnum-1);
     height = h * heightstep;
     for w = 0:(widthnum-1);
@@ -162,7 +167,6 @@ function  [apple_base, appleEmbedded] = embedControllerImage(apple, varargin)
         %iterate through each pixel of block
         for y = width + 1 : width + widthstep,
         for x = height + 1 : height + heightstep,
-            %fprintf('%d %d %d %d\n', x,y,w,h);
             % mask is the weights of the nine adjacent blocks for each
             % pixel in a block.
             % we sum the product of the message of adjacent blocks and
@@ -170,41 +174,41 @@ function  [apple_base, appleEmbedded] = embedControllerImage(apple, varargin)
             pixelvalue = 0;
             tot = 0;
             if (h > 0) && (w > 0),
-                pixelvalue =  pixelvalue + message((h-1) * heightnum + w-1 + 1) * mask(1, x-height, y-width);
+                pixelvalue =  pixelvalue + message((h-1)+ 1, w-1 + 1) * mask(1, x-height, y-width);
                 tot = tot + mask(1, x-height, y-width);
             end
             if w > 0,
-                pixelvalue =  pixelvalue + message(h * heightnum + w-1 + 1) * mask(4, x-height, y-width);
+                pixelvalue =  pixelvalue + message(h + 1, w-1 + 1) * mask(4, x-height, y-width);
                 tot = tot + mask(4, x-height, y-width);
             end
-            if (h < (heightnum - 1)) && (w > 0) && length(message) >= ((h+1) * heightnum + w-1 + 1),
-                pixelvalue =  pixelvalue + message((h+1) * heightnum + w-1 + 1) * mask(7, x-height, y-width);
+            if (h < (heightnum - 1)) && (w > 0),
+                pixelvalue =  pixelvalue + message((h+1) + 1, w-1 + 1) * mask(7, x-height, y-width);
                 tot = tot + mask(7, x-height, y-width);
             end
 
             if h > 0,
-                pixelvalue =  pixelvalue + message((h-1) * heightnum + w + 1) * mask(2, x-height, y-width);
+                pixelvalue =  pixelvalue + message((h-1) + 1, w + 1) * mask(2, x-height, y-width);
                 tot = tot + mask(2, x-height, y-width);
             end
             if 1,
-                pixelvalue =  pixelvalue + message(h * heightnum + w + 1) * mask(5, x-height, y-width);
+                pixelvalue =  pixelvalue + message(h + 1, w + 1) * mask(5, x-height, y-width);
                 tot = tot + mask(5, x-height, y-width);
             end
-            if h < (heightnum - 1) && length(message) >= ((h+1) * heightnum + w + 1),
-                pixelvalue =  pixelvalue + message((h+1) * heightnum + w + 1) * mask(8, x-height, y-width);
+            if h < (heightnum - 1),
+                pixelvalue =  pixelvalue + message((h+1)+ 1, w + 1) * mask(8, x-height, y-width);
                 tot = tot + mask(8, x-height, y-width);
             end
 
             if (h > 0) && (w < (widthnum - 1)),
-                pixelvalue =  pixelvalue + message((h-1) * heightnum + w+1 + 1) * mask(3, x-height, y-width);
+                pixelvalue =  pixelvalue + message((h-1)+ 1, w+1 + 1) * mask(3, x-height, y-width);
                 tot = tot + mask(3, x-height, y-width);
             end
             if w < (widthnum - 1),
-                pixelvalue =  pixelvalue + message(h * heightnum + w+1 + 1) * mask(6, x-height, y-width);
+                pixelvalue =  pixelvalue + message(h + 1, w+1 + 1) * mask(6, x-height, y-width);
                 tot = tot + mask(6, x-height, y-width);
             end
-            if (h < (heightnum - 1)) && (w < (widthnum - 1)) && length(message) >= ((h+1) * heightnum + w+1 + 1),
-                pixelvalue =  pixelvalue + message((h+1) * heightnum + w+1 + 1) * mask(9, x-height, y-width);
+            if (h < (heightnum - 1)) && (w < (widthnum - 1)),
+                pixelvalue =  pixelvalue + message((h+1)+ 1, w+1 + 1) * mask(9, x-height, y-width);
                 tot = tot + mask(9, x-height, y-width);
             end
 
@@ -219,9 +223,10 @@ function  [apple_base, appleEmbedded] = embedControllerImage(apple, varargin)
     end
     end
     
+    figure,imshow(blurred)
+    
     %% Embedd checkerboard pattern via addition
     appleEmbedded = zeros(size(apple));
-    whos apple_delta apple_base blurred;
     for i = 1:3 %For each of 3 RGB color channels
         appleEmbedded(:,:,i) = apple_base(:,:,i) + blurred.*apple_delta(:,:,i)*delta_length;    
     end
@@ -231,26 +236,24 @@ function  [apple_base, appleEmbedded] = embedControllerImage(apple, varargin)
     appleEmbedded(appleEmbedded>1.0) = 1.0;
 
     
-    figure, imshow(apple_base)'
+    figure, imshow(apple_base)
     %% Generate video for human vision tests
     % :2:totalFrames
     %%{
     fps = 6;
-    durration = 30; %video durration in seconds
+    durration = 10; %video durration in seconds
     numFrames = fps*durration;
 
     prompt = ['\n - Press <Enter> to Play Video - \n'];
     input(prompt);
 
-    imwrite(apple_base, 'vmimo1.bmp');
-    imwrite(appleEmbedded, 'vmimo3.bmp');
-    imwrite(apple_base, 'vmimo2.bmp');
-    imwrite(appleEmbedded, 'vmimo4.bmp');
-    
     frames(1) = im2frame(apple_base);
     frames(2) = im2frame(appleEmbedded);
 
+    imwrite(apple_base, 'vmimo1.bmp');
+    imwrite(appleEmbedded, 'vmimo3.bmp');
+    
     writeFrames(frames, 'sampleVid.avi', fps);
     
-    figure(640), movie(frames,numFrames,fps);
+    figure(200), movie(frames,numFrames,fps);
     %%}
